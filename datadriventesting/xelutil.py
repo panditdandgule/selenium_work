ff7a874b50df2916a84dd692502a151476d9062c
pandit dandgule <panditdandgule777@gmail.com>
2022-03-03 09:17:51 +0530

    data driven testing.

---
+++ datadriventesting/xelutil.py
@@ -0,0 +1,23 @@
+import openpyxl
+
+def getRowCount(file,sheetName):
+    workbook=openpyxl.load_workbook(file)
+    sheet=workbook.get_sheet_by_name(sheetName)
+    return(sheet.max_row)
+
+def getColCount(file,sheetName):
+    workbook=openpyxl.load_workbook(file)
+    sheet=workbook.get_sheet_by_name(sheetName)
+    return(sheet.max_column)
+
+def readData(file,sheetName,rownum,columno):
+    workbook=openpyxl.load_workbook(file)
+    sheet=workbook.get_sheet_by_name(sheetName)
+    return sheet.cell(row=rownum,column=columno).value
+
+def writeData(file,sheetName,rownum,columno,data):
+    workbook=openpyxl.load_workbook(file)
+    sheet=workbook.get_sheet_by_name(sheetName)
+    sheet.cell(row=rownum,column=columno).value=data
+    workbook.save(file)
+
